97ba5055307a0dbb70b3d4b895c8118165e84135
Angie Palos <saulblue30@gmail.com>
2023-05-21 15:40:26 -0600

    envn't

--- src/app.js
+++ src/app.js
@@ -12,11 +12,11 @@ const { Session } = require('express-session');
 //Define que si hay un valor existente lo uso sino usa el ya definido
 const PORT = process.env.PORT || 3000 ;
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || '';
-const DB_NAME = process.env.DB_NAME || 'sistema3';
-const DB_PORT = process.env.DB_PORT || 3306 ;
+const DB_HOST='containers-us-west-200.railway.app';
+const DB_NAME='railway';
+const DB_PASSWORD='BTbw7G1HazDtE0bNNGAW';
+const DB_PORT=7911;
+const DB_USER='root';
 
 console.log(DB_HOST,' ',DB_NAME,' ',DB_PASSWORD,' ',DB_PORT,' ',DB_USER);
 
